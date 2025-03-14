dfb3962f91348468e8080ca10da120d4e3e301ca
Long Teng <uestc.longteng@gmail.com>
2025-03-14 17:54:17 +0100

    new setup script

--- conda.sh
+++ conda.sh
@@ -1,7 +1,4 @@
-# pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu126
-# pip install bitsandbytes
-pip install diffusers transformers accelerate bitsandbytes peft xformers -U
 
+# pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu126
 # conda install conda-forge::transformers conda-forge::xformers conda-forge::accelerate conda-forge::peft conda-forge::bitsandbytes -y
-conda install conda-forge::huggingface_hub conda-forge::datasets conda-forge::tqdm conda-forge::wandb -y
+conda install conda-forge::pytorch conda-forge::torchvision conda-forge::torchaudio conda-forge::transformers conda-forge::diffusers conda-forge::accelerate conda-forge::peft conda-forge::huggingface_hub conda-forge::datasets conda-forge::tqdm conda-forge::wandb conda-forge::xformers conda-forge::bitsandbytes -y
