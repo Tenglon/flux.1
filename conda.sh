b5ca79f0d67f9e57d6acd01bdd16dfa6a1458ca1
Tenglon <62259673+Tenglon@users.noreply.github.com>
2025-11-03 15:39:04 +0100

    Update conda.sh

--- conda.sh
+++ conda.sh
@@ -1,5 +1,7 @@
 
 pip3 install torch torchvision torchaudio # 12.4
+# For LUMI
+# pip3 install torch torchvision --index-url https://download.pytorch.org/whl/rocm6.4
 # pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu126
 pip install diffusers transformers accelerate bitsandbytes peft xformers -U
 
